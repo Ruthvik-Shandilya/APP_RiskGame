a288deafa9136bb383cd43fcb4bc1f502b893c48
Karandeep Singh <terminatorkaran@gmail.com>
2018-10-02 12:31:41 -0400

    Added model objects in MapValidation

--- src/com/risk/services/MapValidate.java
+++ src/com/risk/services/MapValidate.java
@@ -7,31 +7,59 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import com.risk.model.Country;
+import com.risk.model.Continent;
 
 public class MapValidate {
 
-    private HashSet<String> continentSetOfContinents;
+    private HashMap<String, Continent> continentSetOfContinents;
 
-    private HashSet<String> continentSetOfTerritories;
+    private HashMap<String, Continent> continentSetOfTerritories;
 
-    private HashMap<String,ArrayList<String>> adjacentCountries;
+    private HashMap<String, Country> countrySet;
 
-    private HashMap<String, HashSet<String>> countriesInContinent;
+    private HashMap<Country,ArrayList<Country>> adjacentCountries;
+
+    private HashMap<Continent, HashSet<Country>> countriesInContinent;
+
+    private String fileName;
+
+    private ArrayList<String> mapTagData;
 
     public MapValidate(){
-        this.continentSetOfContinents = new HashSet<>();
-        this.continentSetOfTerritories = new HashSet<>();
+        this.continentSetOfContinents = new HashMap<>();
+        this.continentSetOfTerritories = new HashMap<>();
         this.adjacentCountries = new HashMap<>();
         this.countriesInContinent = new HashMap<>();
+        this.mapTagData = new ArrayList<>();
+        this.countrySet = new HashMap<>();
+    }
+
+    public HashMap<String, Continent> getContinentSetOfContinents() {
+        return continentSetOfContinents;
+    }
+
+    public HashMap<Country, ArrayList<Country>> getAdjacentCountries() {
+        return adjacentCountries;
+    }
+
+    public HashMap<Continent, HashSet<Country>> getCountriesInContinent() {
+        return countriesInContinent;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public ArrayList<String> getMapTagData() {
+        return mapTagData;
     }
 
     public boolean validateMapFile(String mapFile){
+        this.fileName = mapFile;
         if(mapFile!=null){
             try(BufferedReader read = new BufferedReader(new FileReader(mapFile))) {
                 String inputText = new String(Files.readAllBytes(Paths.get(mapFile)), StandardCharsets.UTF_8);
@@ -45,10 +73,10 @@ public class MapValidate {
                         while (!((line = read.readLine()).equals("[Continents]"))) {
                             line = read.readLine();
                             if (!line.trim().isEmpty() && !(line.contains("="))) {
-                                System.out.println("line=" + line);
                                 System.out.print("Invalid map configuration");
                                 return false;
                             }
+                            mapTagData.add(line);
                         }
                     }
                     if (line.equals("[Continents]")) {
@@ -63,36 +91,76 @@ public class MapValidate {
                                     System.out.print("Invalid continent configuration");
                                     return false;
                                 }
-                                if (continentSetOfContinents.contains(line.split("=")[0])) {
+
+                                if (continentSetOfContinents.containsKey(line.split("=")[0])) {
                                     System.out.println("Continent is already defined.");
                                     return false;
-                                } else {
-                                    continentSetOfContinents.add(line.split("=")[0]);
                                 }
+                                Continent continent = new Continent(line.split("=")[0], Integer.parseInt(line.split("=")[1]));
+                                continentSetOfContinents.put(continent.getName(), continent);
                             }
-
                         }
                     }
                     if (line.equals("[Territories]")) {
                         while ((line = read.readLine()) != null) {
                             if (!line.trim().isEmpty()) {
                                 String input[] = line.split(",");
-                                continentSetOfTerritories.add(input[3].trim());
-                                if (!countriesInContinent.containsKey(input[3])) {
-                                    HashSet<String> countries = new HashSet<>();
-                                    countries.add(input[0].trim());
-                                    countriesInContinent.put(input[3].trim(), countries);
-                                } else {
-                                    HashSet<String> countries = countriesInContinent.get(input[3].trim());
-                                    countries.add(input[0].trim());
-                                    countriesInContinent.put(input[3].trim(), countries);
+                                if(continentSetOfTerritories.get(input[3].trim())== null) {
+                                    Continent continent = new Continent(input[3].trim(), 0);
+                                    continentSetOfTerritories.put(continent.getName(), continent);
                                 }
-                                ArrayList<String> countries = new ArrayList<>();
-                                for (int i = 4; i < input.length; ++i) {
-                                    countries.add(input[i].trim());
-                                }
-                                adjacentCountries.put(input[0].trim(), countries);
+                                Country country;
+                                if (!countriesInContinent.keySet().contains(continentSetOfTerritories.get(input[3]))) {
+                                    HashSet<Country> countries = new HashSet<>();
+                                    if(!countrySet.containsKey(input[0].trim())) {
+                                        country = new Country(input[0].trim());
+                                        country.setContinent(continentSetOfTerritories.get(input[3]).getName());
+                                        country.setxValue(input[1]);
+                                        country.setyValue(input[2]);
+                                        countrySet.put(input[0].trim(),country);
+                                    }
 
+                                    else {
+                                        country = countrySet.get(input[0].trim());
+                                        country.setContinent(continentSetOfTerritories.get(input[3]).getName());
+                                        country.setxValue(input[1]);
+                                        country.setyValue(input[2]);
+                                    }
+
+                                    countries.add(country);
+                                    countriesInContinent.put(continentSetOfTerritories.get(input[3]), countries);
+                                } else {
+                                    HashSet<Country> countries = getCountriesFromContinent(input[3].trim(),countriesInContinent);
+                                    if(!countrySet.containsKey(input[0].trim())) {
+                                        country = new Country(input[0].trim());
+                                        country.setxValue(input[1]);
+                                        country.setyValue(input[2]);
+                                        country.setContinent(continentSetOfTerritories.get(input[3]).getName());
+                                        countrySet.put(input[0].trim(),country);
+                                    }
+                                    else {
+                                        country = countrySet.get(input[0].trim());
+                                        country.setContinent(continentSetOfTerritories.get(input[3]).getName());
+                                        country.setxValue(input[1]);
+                                        country.setyValue(input[2]);
+                                    }
+                                    countries.add(country);
+                                    countriesInContinent.put(continentSetOfTerritories.get(input[3]), countries);
+                                }
+                                ArrayList<Country> countries = new ArrayList<>();
+                                for (int i = 4; i < input.length; ++i) {
+                                    Country adjacentCountry;
+                                    if(!countrySet.containsKey(input[i].trim())){
+                                        adjacentCountry = new Country(input[i].trim());
+                                        countrySet.put(input[i].trim(),adjacentCountry);
+                                    }
+
+                                    else{
+                                        adjacentCountry = countrySet.get(input[i].trim());
+                                    }
+                                    countries.add(adjacentCountry);
+                                }
+                                adjacentCountries.put(country, countries);
                             }
                         }
                     }
@@ -103,7 +171,7 @@ public class MapValidate {
             }
 
 
-        for(Map.Entry<String,HashSet<String>> countries: countriesInContinent.entrySet()){
+        for(Map.Entry<Continent,HashSet<Country>> countries: countriesInContinent.entrySet()){
             if(countries.getValue().size() < 1){
                 System.out.println(countries);
                 System.out.println("Number of countries in a continent is less");
@@ -111,10 +179,10 @@ public class MapValidate {
             }
         }
 
-        for(Map.Entry<String,ArrayList<String>> countries: adjacentCountries.entrySet()){
-            String checkCountry = countries.getKey();
-            ArrayList<String> neighbours = countries.getValue();
-            for(String adjacent : neighbours){
+        for(Map.Entry<Country,ArrayList<Country>> countries: adjacentCountries.entrySet()){
+            Country checkCountry = countries.getKey();
+            ArrayList<Country> neighbours = countries.getValue();
+            for(Country adjacent : neighbours){
                 if(!adjacentCountries.get(adjacent).contains(checkCountry)){
                     System.out.println("Countries are not adjacent");
                     return false;
@@ -132,16 +200,25 @@ public class MapValidate {
         return true;
     }
 
+    public HashSet<Country> getCountriesFromContinent(String continent, HashMap<Continent,HashSet<Country>> hm){
+        for(Map.Entry<Continent,HashSet<Country>> pair : hm.entrySet()){
+            if(pair.getKey().getName().equals(continent)){
+                return pair.getValue();
+            }
+        }
+        return null;
+    }
+
     public boolean checkAllTags(String fileData){
-        if(!fileData.contains("[Map]")|| countOccurrences(fileData,"[Map")!=1){
+        if(!fileData.contains("[Map]")|| countOccurrences(fileData,"[Map]")!=1){
             return false;
         }
 
-        else if(!fileData.contains("[Continents]")|| countOccurrences(fileData,"[Continents")!=1){
+        else if(!fileData.contains("[Continents]")|| countOccurrences(fileData,"[Continents]")!=1){
             return false;
         }
 
-        else if(!fileData.contains("[Territories]")|| countOccurrences(fileData,"[Territories")!=1){
+        else if(!fileData.contains("[Territories]")|| countOccurrences(fileData,"[Territories]")!=1){
             return false;
         }
 
