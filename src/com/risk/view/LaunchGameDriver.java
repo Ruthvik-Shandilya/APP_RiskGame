9f810de3aa4cec55c2e48ff839d1b8636b904645
Karandeep Singh <terminatorkaran@gmail.com>
2018-10-05 14:38:03 -0400

    Added File Chooser window

--- src/com/risk/view/LaunchGameDriver.java
+++ src/com/risk/view/LaunchGameDriver.java
@@ -1,5 +1,7 @@
 package com.risk.view;
 
+import com.risk.services.MapIO;
+import com.risk.services.MapValidate;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -10,11 +12,21 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
+/**
+ * Launches the main window and provides the view for the user, either to load or create a new map.
+ *
+ * @author karandeep
+ * @author ruthvik
+ */
 
+public class LaunchGameDriver extends Application {
 
-class GameDriver extends Application {
+    /**
+     * Declare buttons for loading and creating a map file.
+     */
+    Button loadMapButton, createMapButton;
 
-    Button loadMap, createMap;
+    MapIO readMap;
 
     public static void main(String[] args) {
         launch(args);
@@ -27,30 +39,46 @@ class GameDriver extends Application {
 
         Label gameLabel = new Label("Risk Game");
         gameLabel.setStyle("-fx-font-weight: bold");
-        gameLabel.setFont(new Font("Arial",30));
+        gameLabel.setFont(new Font("Arial", 30));
         gameLabel.setTextFill(Color.RED);
 
         Label optionLabel = new Label("Please select below options");
-        optionLabel.setFont(new Font("Arial",20));
+        optionLabel.setFont(new Font("Arial", 20));
         optionLabel.setTextFill(Color.RED);
 
-        loadMap = new Button();
-        loadMap.setText("Load Map File");
+        loadMapButton = new Button();
+        loadMapButton.setText("Load Map File");
+        loadMapButton.setOnAction(e -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Select a Map File");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Map File Extensions", "*.map", "*.MAP"));
+            File selectedFile = fileChooser.showOpenDialog(null);
+            if (selectedFile != null) {
+                loadMapButton.getScene().getWindow().hide();
+                String fileName = selectedFile.getAbsolutePath();
+                System.out.println("File location: " + fileName);
+                MapValidate mapValidate = new MapValidate();
+                if (mapValidate.validateMapFile(fileName)) {
+                    //readMap = new MapIO(mapValidate);
+                    //readMap.readFile();
+                    //new MapLoader().validateAndLoadMapFile(fileName);
+                }
+            }
+        });
 
-        createMap = new Button();
-        createMap.setText("Create a New Map");
+        createMapButton = new Button();
+        createMapButton.setText("Create a New Map");
 
-        loadMap.setMaxWidth(150);
-        createMap.setMaxWidth(150);
+        loadMapButton.setMaxWidth(150);
+        createMapButton.setMaxWidth(150);
 
         VBox layout = new VBox();
         layout.setStyle("-fx-background-color: black");
         layout.setSpacing(20);
-        //layout.setPadding(new Insets(0,10,10,10));
         layout.setAlignment(Pos.CENTER);
-        layout.getChildren().addAll(gameLabel, optionLabel, loadMap,createMap);
+        layout.getChildren().addAll(gameLabel, optionLabel, loadMapButton, createMapButton);
 
-        Scene scene = new Scene(layout,400,400);
+        Scene scene = new Scene(layout, 400, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
 
