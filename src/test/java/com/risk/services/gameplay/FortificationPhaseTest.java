a2be823cd637d305ea19c0291d0eb04bb27ed27b
neha08585 <32138361+neha08585@users.noreply.github.com>
2018-10-08 22:50:11 -0400

    Test Files for fortifaction Phase and Reinforcement Phase

---
+++ src/test/java/com/risk/services/gameplay/FortificationPhaseTest.java
@@ -0,0 +1,66 @@
+package com.risk.services.gameplay;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.risk.model.Country;
+
+/**
+ * Test class for fortification phase.
+ * 
+ * @author Neha Pal
+ *
+ */
+
+class FortificationPhaseTest {
+
+	/** Object for FortificationPhase class */
+	private FortificationPhase fortificationPhase;
+	
+	/** First Object1 for Country class */
+	private Country country1;
+
+	/** Second Object for Country Class  */
+	private Country country2;
+
+	/** Number of armies */
+	private int numberOfArmies;
+
+	/** ArrayList to hold adjacent countries for  */
+	private ArrayList<Country> adjacentCountries;
+
+	/**
+	 * Set up the initial objects for Fortification Phase
+	 * @throws Exception
+	 */
+	@BeforeEach
+	
+	public void setUp() throws Exception {
+		country1 = new Country("C1");
+		country2 = new Country("C2");
+		country1.setNoOfArmies(5);
+		country2.setNoOfArmies(7);
+		fortificationPhase = new FortificationPhase();
+		adjacentCountries = new ArrayList<Country>();
+		adjacentCountries.add(country2);
+		country1.setAdjacentCountries(adjacentCountries);
+		numberOfArmies = 1;
+
+	}
+
+	/**
+	 * Test to validate number of armies after the player moves fix number of armies.
+	 * between two adjacent countries owned by player
+	 */
+	
+	@Test
+	public void moveArmiesTest() throws Exception {
+		fortificationPhase.moveArmies(country1, country2, numberOfArmies);
+		assertEquals(4,country1.getNoOfArmies());
+		assertEquals(8,country2.getNoOfArmies());
+	}
+}
---
+++ src/test/java/com/risk/services/gameplay/ReinforcementPhaseTest.java
@@ -0,0 +1,90 @@
+package com.risk.services.gameplay;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.risk.model.Continent;
+import com.risk.model.Country;
+import com.risk.model.Player;
+
+/**
+ * Test class for Reinforcement Phase.
+ * 
+ * @author Neha Pal
+ *
+ */
+
+class ReinforcementPhaseTest {
+
+	/** Object for ReinforcementPhase class */
+	private ReinforcementPhase reinforcementPhase;
+
+	/** Object for Player class */
+	private Player player;
+
+	/** Object for Country class */
+	private Country country;
+
+	/** Object for Continent class */
+	private Continent continent;
+
+	/** ArrayList to hold countries owned by the player */
+	private ArrayList<Country> playerOwnedContries;
+
+	/** ArrayList to hold list of countries in continent */
+	private ArrayList<Country> continentListOfCountries;
+
+	/**
+	 * Set up the initial objects for Reinforcement Phase
+	 * 
+	 * @throws Exception
+	 */
+
+	@BeforeEach
+	void setUp() throws Exception {
+		reinforcementPhase = new ReinforcementPhase();
+
+		playerOwnedContries = new ArrayList<Country>();
+		continentListOfCountries = new ArrayList<Country>();
+		country = new Country("C1");
+		playerOwnedContries.add(country);
+		continentListOfCountries.add(country);
+
+		country = new Country("C2");
+		playerOwnedContries.add(country);
+		continentListOfCountries.add(country);
+
+		country = new Country("C3");
+		playerOwnedContries.add(country);
+		continentListOfCountries.add(country);
+
+		player = new Player(playerOwnedContries);
+		continent = new Continent("Europe", 2);
+
+		continent.setListOfCountries(continentListOfCountries);
+	}
+
+	/**
+	 * Test to validate number of armies when the whole continent is owned by the player
+	 */
+
+	@Test
+	void testFindNoOfArmiesWhenPlayerOwnContinent() {
+		assertEquals(continent.getControlValue(), reinforcementPhase.findNoOfArmies(player, continent));
+	}
+
+	
+	/**
+	 * Test to validate number of armies when player does not owns the continent
+	 */
+	@Test
+	void testFindNoOfArmiesWhenPlayerDoesNotOwnContinent() {
+
+		country = new Country("C4");
+		continentListOfCountries.add(country);
+		assertEquals(3, reinforcementPhase.findNoOfArmies(player, continent));
+	}
+}
