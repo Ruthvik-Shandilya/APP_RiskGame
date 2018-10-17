0c2a0507be9b0c811a4727106d458797fb123a1f
Jehlum <k2@karakrum>
2018-10-17 15:28:07 -0400

    Javadoc added to MapGraphTest file

--- src/test/java/com/risk/services/MapGraphTest.java
+++ src/test/java/com/risk/services/MapGraphTest.java
@@ -12,18 +12,30 @@ import org.junit.Test;
 import com.risk.model.Continent;
 import com.risk.model.Country;
 
+/**
+ * Test Class for MapValidation
+ *
+ * @author Farhan Shaheen
+ *
+ */
+ 
 public class MapGraphTest {
 
+	/** Object for Continent class*/
 	private Continent continent;
-
+	/** Object for MapGraph class*/
 	private MapGraph mapGraph;
-
+	/** Object for Country class*/
 	private Country country;
-
+	/** Adjacent countries*/
 	private HashMap<Country, ArrayList<Country>> adjacentCountries;
-
+	/** List of countries*/
 	private ArrayList<Country> countryList;
 
+	/**
+	* Set up the initial objects for MapGraph
+	* @throws Exception
+	*/
 	@Before
 	public void setUp() throws Exception {
 		continent = new Continent("Asia", 2);
@@ -37,14 +49,22 @@ public class MapGraphTest {
 
 	}
 
+	/**
+	* Test method for testing adding a continent from the map
+	* @throws Exception
+	*/
 	@Test
-	public void testAddContinent() {
+	public void testAddContinent() throws Exception{
 		mapGraph.addContinent(continent);
 		assertEquals(mapGraph.getContinents().get("Asia").getControlValue(), continent.getControlValue());
 	}
 
+	/**
+	* Test method for testing removing a continent from the map
+	* @throws Exception
+	*/
 	@Test
-	public void testRemoveContinent() {
+	public void testRemoveContinent() throws Exception{
 		mapGraph.addContinent(continent);
 		assertTrue(mapGraph.removeContinent(continent));
 	}
