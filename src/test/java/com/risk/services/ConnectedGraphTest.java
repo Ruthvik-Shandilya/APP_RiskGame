cd9f72b62315e03bce10507458c9f22d2148c055
Karandeep Singh <terminatorkaran@gmail.com>
2018-10-19 19:38:21 -0400

    Added ConnectedGraph Test File

--- src/test/java/com/risk/services/ConnectedGraphTest.java
+++ src/test/java/com/risk/services/ConnectedGraphTest.java
@@ -20,37 +20,37 @@ public class ConnectedGraphTest {
 
 	/**Object for ConnectedGraph Class*/
 	private ConnectedGraph connectedGraph;
-	
+
 	/** Objects for Country Class */
 	private Country country1, country2, country3;
-	
+
 	/**Set to store countries*/
 	private Set<Country> countrySet;
-	
+
 	/**ArrayList to store adjacent countries*/
 	private ArrayList<Country> adjacentCountries;
-		
+
 	/**
-	* Set up the initial objects for ConnectedGraph
-	* @throws Exception
-	*/
+	 * Set up the initial objects for ConnectedGraph
+	 * @throws Exception
+	 */
 	@Before
-	public void setUp() throws Exception {
-		
+	public void initialize() throws Exception {
+
 		country1= new Country("India");
 		country2 = new Country("China");
 		country3 =new Country("Indonesia");
-		
+
 		countrySet = new HashSet<Country>();
 		countrySet.add(country1);
 		countrySet.add(country2);
 		countrySet.add(country3);
-		
+
 		adjacentCountries = new ArrayList<>();
 		adjacentCountries.add(country1);
 		adjacentCountries.add(country2);
 		adjacentCountries.add(country3);
-		
+
 		connectedGraph = new ConnectedGraph(countrySet);
 		country1.setAdjacentCountries(adjacentCountries);
 		country2.setAdjacentCountries(adjacentCountries);
@@ -61,10 +61,10 @@ public class ConnectedGraphTest {
 	 * Test method to check if a Graph is connected
 	 */
 	@Test
-	public void testIsConnected() {
-		
+	public void isConnectedTest() {
+
 		assertTrue(connectedGraph.isConnected());
-		
+
 	}
 
 }
--- src/test/java/com/risk/testsuite/JunitTestSuite.java
+++ src/test/java/com/risk/testsuite/JunitTestSuite.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.risk.services.ConnectedGraphTest;
 import com.risk.services.MapEditorTest;
 import com.risk.services.MapGraphTest;
 import com.risk.services.MapValidateTest;
@@ -15,8 +16,8 @@ import com.risk.services.gameplay.RoundRobinTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({MapEditorTest.class,MapGraphTest.class,MapValidateTest.class,
-			   FortificationPhaseTest.class,ReinforcementPhaseTest.class,
-			   RoundRobinTest.class})
+	FortificationPhaseTest.class,ReinforcementPhaseTest.class,
+	RoundRobinTest.class,ConnectedGraphTest.class})
 
 /**
  * TestSuite Class to test all test cases
