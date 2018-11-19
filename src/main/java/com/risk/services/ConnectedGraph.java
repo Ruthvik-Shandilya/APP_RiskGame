3574cc554f59518bb5893ddd8382355ca29dede6
Karandeep Singh <terminatorkaran@gmail.com>
2018-11-19 15:56:16 -0500

    Added connected subgraph

--- src/main/java/com/risk/services/ConnectedGraph.java
+++ src/main/java/com/risk/services/ConnectedGraph.java
@@ -9,27 +9,28 @@ import com.risk.model.Country;
 /**
  * Class containing the method to check whether a map is a connected graph or
  * not
- * 
+ *
  * @author Karandeep Singh
  * @author Ruthvik Shandilya
- * 
  */
 
 public class ConnectedGraph {
 
-	/** HashMap for marking a country as visited or not */
+	/**
+	 * HashMap for marking a country as visited or not
+	 */
 	private HashMap<Country, Boolean> visited;
 
-	/** Set containing countries */
+	/**
+	 * Set containing countries
+	 */
 	private Set<Country> countrySet;
 
 	/**
 	 * Constructor for ConnectedGraph class, which sets initial data for the
 	 * class. Moreover, it sets a the countries visited value as false.
-	 * 
-	 * @param countrySet
-	 *            Set containing all the countries.
-	 * 
+	 *
+	 * @param countrySet Set containing all the countries.
 	 */
 	public ConnectedGraph(Set<Country> countrySet) {
 		this.countrySet = countrySet;
@@ -42,9 +43,8 @@ public class ConnectedGraph {
 
 	/**
 	 * Method for traversing the graph for depth first traversal
-	 * 
+	 *
 	 * @param startCountry
-	 * 
 	 */
 	private void depthFirstTraversal(Country startCountry) {
 		visited.put(startCountry, true);
@@ -59,9 +59,8 @@ public class ConnectedGraph {
 
 	/**
 	 * Method for checking if the graph is connected or not.
-	 * 
+	 *
 	 * @return true if map is connect; else false.
-	 * 
 	 */
 	public boolean isConnected() {
 		depthFirstTraversal(countrySet.iterator().next());
@@ -74,5 +73,31 @@ public class ConnectedGraph {
 			}
 		}
 		return true;
+	}
+
+	public boolean isConnectedSubGraph() {
+		depthFirstTraversalSubGraph(countrySet.iterator().next());
+		Iterator<Country> it = countrySet.iterator();
+		while (it.hasNext()) {
+			Country country = it.next();
+			if (visited.get(country) == false) {
+				System.out.println("Continent " + country.getContinent() + " is not connected.");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void depthFirstTraversalSubGraph(Country startCountry) {
+		visited.put(startCountry, true);
+		Iterator<Country> it = startCountry.getAdjacentCountries().iterator();
+		while (it.hasNext()) {
+			Country adjacentCountry = it.next();
+			if (countrySet.contains(adjacentCountry)) {
+				if (!visited.get(adjacentCountry)) {
+					depthFirstTraversalSubGraph(adjacentCountry);
+				}
+			}
+		}
 	}
 }
