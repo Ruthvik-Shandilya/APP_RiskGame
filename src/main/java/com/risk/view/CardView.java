419b1f9e6e6b3e4d1ab9002080056828d68eb3c2
Karandeep Singh <terminatorkaran@gmail.com>
2018-11-20 23:38:03 -0500

    Added card view

---
+++ src/main/java/com/risk/view/CardView.java
@@ -0,0 +1,30 @@
+package com.risk.view;
+
+import com.risk.model.Player;
+import com.risk.view.controller.CardController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CardView {
+
+    public static void openCardWindow(Player currentPlayer, com.risk.model.Card card) {
+        final Stage newCardStage = new Stage();
+        newCardStage.setTitle("Card Window");
+        CardController cardController = new CardController(currentPlayer, card);
+        FXMLLoader loader = new FXMLLoader(CardView.class.getClassLoader().getResource("Cards.fxml"));
+        loader.setController(cardController);
+        Parent root = null;
+        try {
+            root = (Parent) loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        newCardStage.setScene(scene);
+        newCardStage.show();
+    }
+}
