3fc7a546b2e758e25ad0ad97f43bfa6629f82853
Jehlum <fshaheen.cs@gmail.com>
2018-11-29 20:42:10 -0500

    Player Behaviour JavaDoc

--- src/main/java/com/risk/strategy/PlayerBehaviour.java
+++ src/main/java/com/risk/strategy/PlayerBehaviour.java
@@ -12,19 +12,79 @@ import java.util.List;
 import java.util.Observable;
 import java.util.stream.Collectors;
 
+/**
+ * 
+ * PlayerBehaviour class contains methods for the Player Behavior.
+ * There are five player behaviours 
+ * i.e. Human, aggressive, benevolent, random and cheater
+ * 
+ * @author Karandeep Singh
+ * @author Palash Jain
+ * 
+ */
 public abstract class PlayerBehaviour extends Observable implements Serializable {
 
+    /**
+     * 
+	 * Method for reinforcement phase. 
+	 * Start and end of the reinforcement phase. 
+	 * 
+	 * @param countryList
+	 *            List of countries owned by the player.
+	 * @param country
+	 *            Country to which reinforcement armies are to be assigned.
+	 * @param currentPlayer
+	 *            Current player.
+	 *            
+	 */
     abstract public void reinforcementPhase(ObservableList<Country> countryList, Country country, Player currentPlayer);
 
-
+    /**
+     * 
+	 * Method for attack phase.
+	 * 
+	 * @param attackingCountryList
+	 *            List of countries attacking.
+	 * @param defendingCountryList
+	 *            List of countries defending.
+	 * @param currentPlayer
+	 *            Current player.
+	 *            
+	 */ 
     abstract public void attackPhase(ListView<Country> attackingCountryList, ListView<Country> defendingCountryList,
                                      Player currentPlayer);
 
-
+    /**
+     * 
+	 * Method for fortification phase. 
+	 * Start and end of the fortification phase. 
+	 * 
+	 * @param selectedCountry
+	 *            List of countries selected by the player.
+	 * @param adjCountry
+	 *            List of adjacent countries.
+	 * @param playerPlaying
+	 *            Current player.
+	 * 
+	 * @return true 
+     * 			  If the fortification successful; other wise false.
+     * 
+	 */ 
     abstract public boolean fortificationPhase(ListView<Country> selectedCountry, ListView<Country> adjCountry,
                                                Player playerPlaying);
 
 
+    /**
+     * 
+	 * Method to get list of defending country. 
+	 * 
+	 * @param attackingCountry
+	 *            Attacking country.
+	 *
+	 * @return List 
+     * 			  List of defending countries.
+     * 
+	 */
     public List<Country> getDefendingCountryList(Country attackingCountry) {
         List<Country> defendingCountries = attackingCountry.getAdjacentCountries().stream()
                 .filter(t -> (attackingCountry.getPlayer() != t.getPlayer())).collect(Collectors.toList());
@@ -33,9 +93,32 @@ public abstract class PlayerBehaviour extends Observable implements Serializable
 
     }
 
+    /**
+     * 
+	 * Method for if player can attack.
+	 * 
+	 * @param countries
+	 *            List of countries owned by the player.
+	 *   
+	 * @return true 
+     * 			  If player can attack; other wise false.
+     *            
+	 */    
     abstract public boolean playerCanAttack(ListView<Country> countries);
 
-
+    /**
+     * 
+	 * Method for to check if fortification phase is valid.
+	 * 
+	 * @param mapIO
+	 *            MapIO Object.
+	 * @param playerPlaying           
+	 *   		  Player playing.
+	 *   
+	 * @return true 
+     * 			  If fortification phase is valid; other wise false.
+     *            
+	 */ 
     public boolean isFortificationPhaseValid(MapIO mapIO, Player playerPlaying) {
         boolean isFortificationAvailable = false;
         outer:
