9775d7310b2eee188b3a4f67ced29cd0823cb7f3
Jehlum <fshaheen.cs@gmail.com>
2018-11-30 00:31:46 -0500

    PlayerWorldDomination JavaDoc

--- src/main/java/com/risk/model/PlayerWorldDomination.java
+++ src/main/java/com/risk/model/PlayerWorldDomination.java
@@ -7,8 +7,31 @@ import java.util.Observable;
 
 import com.risk.services.MapIO;
 
+/**
+ * 
+ * Players world domination view class.
+ * Display:
+ * (1) the percentage of the map controlled by every player
+ * (2) the continents controlled by every player 
+ * (3) the total number of armies owned by every player.
+ * 
+ * @author Ruthvik Shandilya
+ * @author Palash Jain
+ * 			  
+ */
 public class PlayerWorldDomination extends Observable implements Serializable {
 
+	/**
+	 * 
+	 * Method for PlayerWorldDomination class to generate world domination data.
+	 * 
+	 * @param map
+	 * 			  MAPIO object.
+	 * 
+	 * @return playerTerPercent
+	 * 			  Percentage of the map controlled by every player.
+	 * 			  
+	 */
 	public HashMap<Player, Double> generateWorldDominationData(MapIO map) {
 
 		HashMap<Player, Double> playerCountryCount = new HashMap<>();
