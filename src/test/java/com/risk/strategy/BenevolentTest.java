18baa890ed2d840b8eb7b0e53d25b8f3fac18203
neha08585 <32138361+neha08585@users.noreply.github.com>
2018-11-30 00:54:05 -0500

    Adding test cases for Benevolent class

---
+++ src/test/java/com/risk/strategy/BenevolentTest.java
@@ -0,0 +1,78 @@
+package com.risk.strategy;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.risk.model.Country;
+import com.risk.model.Player;
+import com.risk.view.controller.GamePlayController;
+
+public class BenevolentTest {
+	
+	private Country country;
+	
+	private Country adjCountry1;
+	
+	private Country adjCountry2;
+	
+	private ArrayList<Country> adjacentCountries;
+	
+	private Player player;
+
+	private Benevolent benevolent;
+	
+	private GamePlayController gamePlayController;
+	
+	private List<Country> list;
+	
+	@Before
+	public void initialize() {
+		player = new Player();
+		country = new Country("India");
+		country.setPlayer(player);
+		
+		list = new ArrayList<Country>();
+		list.add(country);
+		
+		adjCountry1 = new Country("China");
+		adjCountry1.setPlayer(player);
+		
+		adjCountry2 = new Country("Malaysia");
+		adjCountry2.setPlayer(player);
+		
+		adjacentCountries = new ArrayList<Country>();
+		adjacentCountries.add(adjCountry1);
+		adjacentCountries.add(adjCountry2);
+		
+		country.setAdjacentCountries(adjacentCountries);
+		
+		gamePlayController = new GamePlayController();
+		benevolent = new Benevolent(gamePlayController);
+	}
+	
+	@Test
+	public void getStrongestAdjacentCountryTest() {
+		adjCountry1.setNoOfArmies(3);
+		assertEquals(adjCountry1,benevolent.getStrongestAdjacentCountry(country));
+	}
+	
+	@Test
+	public void getNullStrongestAdjacentCountryTest() {
+		adjCountry1.setNoOfArmies(1);
+		assertEquals(null,benevolent.getStrongestAdjacentCountry(country));
+	}
+	
+	@Test
+	public void checkAndFindWeakestIfNoAdjacentCountryToFortifyTest() {
+		list.get(0).setNoOfArmies(3);
+		adjCountry1.setNoOfArmies(3);
+		System.out.println(benevolent.checkAndFindWeakestIfNoAdjacentCountryToFortify(list));
+		assertEquals(list.get(0), benevolent.checkAndFindWeakestIfNoAdjacentCountryToFortify(list));
+	}
+
+}
